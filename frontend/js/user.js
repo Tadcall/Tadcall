ed330a63a5784878815de84b899cc877e10b4caf
Afonso Oliveira <afonso.oliveira@ist.utl.pt>
2015-06-13 19:26:28 +0100

    restrictions creation and listing

--- frontend/js/user.js
+++ frontend/js/user.js
@@ -1,10 +1,10 @@
 var UserModel = function() {
     self = this;
-    self.username = ko.observable;
+    self.username = ko.observable();
     self.realNumbers = ko.observableArray();
     self.virtualNumbers = ko.observableArray();
     this.getNumbers = function() {
-        self.realNumbers([913451234,931236578,963456857]);
-        self.virtualNumbers([342623465, 325476980, 304568293]);
+        self.realNumbers(['913451234','931236578','963456857']);
+        self.virtualNumbers(['342623465', '325476980', '304568293']);
     } // Ensure that "this" is always this view model
 };
