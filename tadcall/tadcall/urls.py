f1eb731c0b291f6c37cdfd794b9aab1758360a16
Alexandre Campos <alexandre@talkdesk.com>
2015-06-13 14:47:14 +0100

    added get user and get link endpoints

--- tadcall/tadcall/urls.py
+++ tadcall/tadcall/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import backend.work
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^get_user/', backend.work.get_user),
 ]
